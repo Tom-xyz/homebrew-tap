10d46c421e11097adb2fb03dd6ba5258c605f3f7
Thomas Boyle <4200802+Tom-xyz@users.noreply.github.com>
2026-04-27 00:00:40 -0600

    Update pingx to v1.2.0

--- Formula/pingx.rb
+++ Formula/pingx.rb
@@ -3,15 +3,15 @@ class Pingx < Formula
 
   desc "Full-screen TUI ping monitor with auto-reconnect and WAN failover detection"
   homepage "https://github.com/Tom-xyz/pingx"
-  url "https://github.com/Tom-xyz/pingx/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "87cfe3d22e6c7d36f7a72f3658e45c7f9a054751354038fba737b592de16f06d"
+  url "https://github.com/Tom-xyz/pingx/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "f8dbeed8f61c54fac92052483f3aafba1ff029f9474228f63ca55ccdf7d99431"
   license "MIT"
 
   depends_on "python@3.12"
 
   resource "rich" do
     url "https://files.pythonhosted.org/packages/c0/8f/0722ca900cc807c13a6a0c696dacf35430f72e0ec571c4275d2371fca3e9/rich-15.0.0.tar.gz"
-    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
+    sha256 "f8dbeed8f61c54fac92052483f3aafba1ff029f9474228f63ca55ccdf7d99431"
   end
 
   def install
